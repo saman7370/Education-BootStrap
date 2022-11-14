a39876f9049936524eda32b257b0b05f0613289d
saman7370 <mehralisaman8@gmail.com>
2022-11-14 18:14:10 +0330

    notification&responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,20 @@ function loaded(){
 
 $(function(){
     setTimeout(loaded , 3000);
+
+    // notification==========>
+
+    $('.custom-dropdown').on('show.bs.dropdown', function() {
+        // $(this).find('.dropdown-menu').first().stop(false, false).slideDown();
+         // $(this).find('.dropdown-menu').addClass('active');
+         var that = $(this);
+        setTimeout(function(){
+            that.find('.dropdown-menu').addClass('active');
+        }, 100);
+        
+    
+      });
+      $('.custom-dropdown').on('hide.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').removeClass('active');
+      });
 })
